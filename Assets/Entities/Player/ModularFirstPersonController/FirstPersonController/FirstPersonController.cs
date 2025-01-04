5ccf9ba3f492c962cf3926e9b48f2fc018c21078
Davin Leong <davinleongg@gmail.com>
2025-01-03 17:13:12 -0800

    rearranged folders and let player look down

--- Assets/Entities/Player/ModularFirstPersonController/FirstPersonController/FirstPersonController.cs
+++ Assets/Entities/Player/ModularFirstPersonController/FirstPersonController/FirstPersonController.cs
@@ -37,6 +37,9 @@ public class FirstPersonController : MonoBehaviour
     // Internal Variables
     private float yaw = 0.0f;
     private float pitch = 0.0f;
+
+    public float initialPitch = -90f; // Add this line to define the initial pitch angle
+
     private Image crosshairObject;
 
     #region Camera Zoom Variables
@@ -165,6 +168,10 @@ public class FirstPersonController : MonoBehaviour
         {
             crosshairObject.gameObject.SetActive(false);
         }
+
+        // Set the initial pitch angle
+        pitch = initialPitch;
+        playerCamera.transform.localEulerAngles = new Vector3(pitch, 0, 0);
 
         #region Sprint Bar
 
@@ -567,7 +574,7 @@ public class FirstPersonController : MonoBehaviour
         GUI.enabled = fpc.cameraCanMove;
         fpc.invertCamera = EditorGUILayout.ToggleLeft(new GUIContent("Invert Camera Rotation", "Inverts the up and down movement of the camera."), fpc.invertCamera);
         fpc.mouseSensitivity = EditorGUILayout.Slider(new GUIContent("Look Sensitivity", "Determines how sensitive the mouse movement is."), fpc.mouseSensitivity, .1f, 10f);
-        fpc.maxLookAngle = EditorGUILayout.Slider(new GUIContent("Max Look Angle", "Determines the max and min angle the player camera is able to look."), fpc.maxLookAngle, 40, 90);
+        fpc.maxLookAngle = EditorGUILayout.Slider(new GUIContent("Max Look Angle", "Determines the max and min angle the player camera is able to look."), fpc.maxLookAngle, 40, 150);
         GUI.enabled = true;
 
         fpc.lockCursor = EditorGUILayout.ToggleLeft(new GUIContent("Lock and Hide Cursor", "Turns off the cursor visibility and locks it to the middle of the screen."), fpc.lockCursor);
